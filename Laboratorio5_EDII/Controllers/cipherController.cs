1ea7840d26249dd34eb7e0fe84782008f1acb957
EbGu3 <eb1993_jar@hotmail.com>
2020-10-28 23:14:35 -0600

    Corregir controllers

--- Laboratorio5_EDII/Controllers/cipherController.cs
+++ Laboratorio5_EDII/Controllers/cipherController.cs
@@ -9,13 +9,32 @@ using Microsoft.AspNetCore.Mvc;
 namespace Laboratorio5_EDII.Controllers
 {
     [Route("api/[controller]")]
-    public class CipherCesarController : Controller
+    public class cipherController : Controller
     {
         // GET: api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
         }
 
         // DELETE api/values/5
--- Laboratorio5_EDII/Controllers/decipherController.cs
+++ Laboratorio5_EDII/Controllers/decipherController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Laboratorio5_EDII.Controllers
 {
     [Route("api/[controller]")]
-    public class CipherZigZagController : Controller
+    public class decipherController : Controller
     {
         // GET: api/values
         [HttpGet]
@@ -18,7 +18,25 @@ namespace Laboratorio5_EDII.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
