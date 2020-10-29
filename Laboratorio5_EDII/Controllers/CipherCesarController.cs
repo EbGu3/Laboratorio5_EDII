1dab6be5a027a4eaf8dea1c0adf2c774e2a6c406
EbGu3 <eb1993_jar@hotmail.com>
2020-10-28 22:23:57 -0600

    Add controllers

---
+++ Laboratorio5_EDII/Controllers/CipherCesarController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Laboratorio5_EDII.Controllers
+{
+    [Route("api/[controller]")]
+    public class CipherCesarController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
