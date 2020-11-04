8485a6f30867ff143c0e657dd9433ac16bdb153d
AlfaRojo <38761407+AlfaRojo@users.noreply.github.com>
2020-11-03 19:37:54 -0600

    Implementación de interfaz de requisitos

--- Laboratorio5_EDII/Models/IRequestModel.cs
+++ Laboratorio5_EDII/Models/IRequestModel.cs
@@ -1,6 +1,12 @@
-﻿namespace Laboratorio5_EDII.Models
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Laboratorio5_EDII.Models
 {
     public interface IRequestModel<T>
     {
+        IFormFile File { get; set; }
+        T Key { get; set; }
+        string Name { get; set; }
+        public string Cifrado { get; set; }
     }
 }
--- Laboratorio5_EDII/Models/Requisitos.cs
+++ Laboratorio5_EDII/Models/Requisitos.cs
@@ -5,7 +5,9 @@ namespace Laboratorio5_EDII.Models
     public class Requisitos : IRequestModel<string>
     {
         public IFormFile File { get; set; }
-        public object Key { get; set; }
+        public string Key { get; set; }
+        public string Cifrado { get; set; }
+        public string Name { get; set; }
         public string Reloj { get; set; }
         public int Ancho { get; set; }
         public int Niveles { get; set; }
