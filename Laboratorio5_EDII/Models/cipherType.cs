29bdaa6a8dcd5edc4d88c127e9c418c25934a66c
EbGu3 <eb1993_jar@hotmail.com>
2020-11-04 23:30:08 -0600

    Modify the class cipherType

--- Laboratorio5_EDII/Models/cipherType.cs
+++ Laboratorio5_EDII/Models/cipherType.cs
@@ -70,9 +70,30 @@ namespace Laboratorio5_EDII.Models
             return false;
         }
 
-                
-        public bool TypeOfFile(Required values) => (Path.GetExtension(values.File.FileName).Equals(".txt")) == true;
-        public bool ContainsData(Required values) => (values.File == null) == false;
+        public bool Get_Cipher(Required values, string Medodo)
+        { 
+            if((TypeOfFile(values) == true) && (ContainsData(values) == true))
+            {
+                switch (Medodo.ToLower())
+                {
+                    case "cesar":
+                        if (values.Key == null || !(int.TryParse(values.Key, out int Key))) { return false; }
+                        else { return true; }
+                        break;
+                    case "zigzag":
+                        if (values.Niveles == 0) { return false}
+                        else { return true; }
+                        break;
+                    case "ruta":
+                        if (values.Reloj != "Abajo") { return false; }
+                        else { return true; }
+                        break;
+                }
+            }
+            return false;
+        }
+        public bool TypeOfFile(Required values) => (Path.GetExtension(values.File.FileName).Equals(".txt"));
+        public bool ContainsData(Required values) => (values.File == null);
         
     }
 }
